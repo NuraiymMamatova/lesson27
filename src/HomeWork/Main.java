df45699d15a331b67e198496c2a3e3203dcf0f54
kaitaloo <mnurajym9@gmail.com>
2022-09-15 14:37:53 +0600

    Initial commit

---
+++ src/HomeWork/Main.java
@@ -0,0 +1,22 @@
+package HomeWork;
+
+import java.util.function.UnaryOperator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UnaryOperator<Double> sqrt = Math::sqrt;
+        System.out.println(sqrt);
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
